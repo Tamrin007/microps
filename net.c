4edf74422d340af3623914679e48291f5c0e68cf
Tamrin007 <tamrin.bitterchococo@gmail.com>
2024-05-18 03:50:21 +0000

    implement step13

--- net.c
+++ net.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "arp.h"
 #include "icmp.h"
 #include "ip.h"
 #include "net.h"
@@ -275,6 +276,11 @@ int net_init(void)
 {
     if (intr_init() == -1) {
         errorf("intr_init() failure");
+        return -1;
+    }
+
+    if (arp_init() == -1) {
+        errorf("arp_init() failure");
         return -1;
     }
 
